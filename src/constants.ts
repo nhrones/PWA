36c646a9dcc966b773c83f8a6615cd6e8879291d
NDH <ndhrones@gmail.com>
2024-12-11 22:27:51 -0500

    update

--- src/constants.ts
+++
@@ -1,8 +0,0 @@
-
-export const DEV = true
-export const PIN = '3913' // set/get from kv
-export const CollectionName = 'PWA'
-export const OrderDirection = {
-   ASC: 'ASC',
-   DESC: 'DESC',
-}
--- src/view/utils.ts
+++
@@ -1,9 +0,0 @@
-// deno-lint-ignore-file no-explicit-any
-
-export const $  = (id: string) => document.getElementById(id)
-
-/** on - adds an event handler to an htmlElement */
-export const on = (elem: any, event: string, listener: any) => {
-   return elem.addEventListener(event, listener)
-}
-
