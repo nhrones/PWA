8bf2d3e45af0b221e778ab0a1b99f7052406bdff
NDH <ndhrones@gmail.com>
2025-01-24 20:11:23 -0500

    update gitignore

--- dist/bundle.js
+++ dist/bundle.js
@@ -2,7 +2,7 @@
 var __defProp = Object.defineProperty;
 var __name = (target, value) => __defProp(target, "name", { value, configurable: true });
 
-// src/signal.ts
+// src/signals.ts
 function buildEventBus() {
   const eventSubscriptions = /* @__PURE__ */ new Map();
   const newEventBus = {
@@ -36,187 +36,21 @@ function buildEventBus() {
           handler(data);
         }
       }
+    },
+    /** xor encryption */
+    xorEncrypt(text) {
+      let result = "";
+      const key = "ndhg";
+      for (let i = 0; i < text.length; i++) {
+        result += String.fromCharCode(text.charCodeAt(i) ^ key.charCodeAt(i % key.length));
+      }
+      return result;
     }
   };
   return newEventBus;
 }
 __name(buildEventBus, "buildEventBus");
-var signal = buildEventBus();
-
-// src/data/kvClient.ts
-var nextMsgID = 0;
-var kvCache;
-var transactions = /* @__PURE__ */ new Map();
-var KvClient = class {
-  static {
-    __name(this, "KvClient");
-  }
-  DEV = false;
-  nextMsgID = 0;
-  querySet = [];
-  transactions;
-  currentPage = 1;
-  focusedRow = null;
-  CTX;
-  ServiceURL;
-  RegistrationURL;
-  /** ctor */
-  constructor(cache, ctx) {
-    this.CTX = ctx;
-    this.DEV = ctx.DEV;
-    this.ServiceURL = ctx.LOCAL_DB ? ctx.LocalDbURL : ctx.RemoteDbURL;
-    this.RegistrationURL = this.ServiceURL + ctx.RpcURL;
-    kvCache = cache;
-    this.transactions = /* @__PURE__ */ new Map();
-  }
-  /** initialize our EventSource and fetch some data */
-  init() {
-    const eventSource = new EventSource(this.RegistrationURL);
-    console.log("CONNECTING");
-    eventSource.addEventListener("open", () => {
-      if (this.DEV) console.log("setting pin");
-      callProcedure(this.ServiceURL, "GET", { key: ["PIN"] }).then((result) => {
-        if (this.DEV) console.log("GET PIN ", result.value);
-        const pin = xorEncrypt(result.value);
-        if (this.DEV) console.log("GET PIN ", pin);
-        this.CTX.PIN = result.value;
-        this.fetchQuerySet();
-      });
-    });
-    eventSource.addEventListener("error", (_e) => {
-      switch (eventSource.readyState) {
-        case EventSource.OPEN:
-          console.log("CONNECTED");
-          break;
-        case EventSource.CONNECTING:
-          console.log("CONNECTING");
-          break;
-        case EventSource.CLOSED:
-          console.log("DISCONNECTED");
-          break;
-      }
-    });
-    eventSource.addEventListener("message", (evt) => {
-      const parsed = JSON.parse(evt.data);
-      const { txID, error, result } = parsed;
-      if (txID === -1) {
-        this.handleMutation(result);
-      }
-      if (!transactions.has(txID)) return;
-      const transaction = transactions.get(txID);
-      transactions.delete(txID);
-      if (transaction) transaction(error, result);
-    });
-  }
-  /**
-   * handle Mutation Event
-   * @param {{ rowID: any; type: any; }} result
-   */
-  handleMutation(result) {
-    console.info(`Mutation event:`, result.type);
-  }
-  /** set Kv Pin */
-  async setKvPin(rawpin) {
-    const pin = xorEncrypt(rawpin);
-    await callProcedure(this.ServiceURL, "SET", { key: ["PIN"], value: pin }).then((_result) => {
-      if (this.DEV) console.log(`Set PIN ${rawpin} to: `, pin);
-    });
-  }
-  /** fetch a querySet */
-  async fetchQuerySet() {
-    await callProcedure(
-      this.ServiceURL,
-      "GET",
-      { key: [this.CTX.dbOptions.schema.dbKey] }
-    ).then((result) => {
-      restoreCache(xorEncrypt(result.value));
-    });
-  }
-  /** get row from key */
-  get(key) {
-    for (let index = 0; index < this.querySet.length; index++) {
-      const element = this.querySet[index];
-      if (element.id === key) return element;
-    }
-  }
-  /** The `set` method mutates - will call the `persist` method. */
-  set(value) {
-    try {
-      callProcedure(
-        this.ServiceURL,
-        "SET",
-        {
-          key: [this.CTX.dbOptions.schema.dbKey],
-          value
-        }
-      ).then((result) => {
-        this.querySet = result.querySet;
-        return this.querySet;
-      });
-    } catch (e) {
-      return { Error: e };
-    }
-  }
-};
-var callProcedure = /* @__PURE__ */ __name((dbServiceURL, procedure, params) => {
-  const txID = nextMsgID++;
-  return new Promise((resolve, reject) => {
-    transactions.set(txID, (error, result) => {
-      if (error)
-        return reject(new Error(error));
-      resolve(result);
-    });
-    fetch(dbServiceURL, {
-      method: "POST",
-      mode: "cors",
-      body: JSON.stringify({ txID, procedure, params })
-    });
-  });
-}, "callProcedure");
-function restoreCache(records) {
-  const pwaObj = JSON.parse(records);
-  kvCache.dbMap = new Map(pwaObj);
-  kvCache.persist();
-  const result = kvCache.hydrate();
-  if (result == "ok") {
-    signal.fire("buildDataTable", "", kvCache);
-  }
-}
-__name(restoreCache, "restoreCache");
-
-// src/utils.ts
-function xorEncrypt(text) {
-  let result = "";
-  const key = "ndhg";
-  for (let i = 0; i < text.length; i++) {
-    result += String.fromCharCode(text.charCodeAt(i) ^ key.charCodeAt(i % key.length));
-  }
-  return result;
-}
-__name(xorEncrypt, "xorEncrypt");
-function backupData(kvCache2) {
-  const jsonData = JSON.stringify(Array.from(kvCache2.dbMap.entries()));
-  const link = document.createElement("a");
-  const file = new Blob([jsonData], { type: "application/json" });
-  link.href = URL.createObjectURL(file);
-  link.download = "backup.json";
-  link.click();
-  URL.revokeObjectURL(link.href);
-}
-__name(backupData, "backupData");
-function restoreData() {
-  const fileload = document.getElementById("fileload");
-  fileload?.click();
-  fileload?.addEventListener("change", function() {
-    const reader = new FileReader();
-    reader.onload = function() {
-      restoreCache(reader.result);
-      globalThis.location.reload();
-    };
-    reader.readAsText(fileload.files[0]);
-  });
-}
-__name(restoreData, "restoreData");
+var signals = buildEventBus();
 
 // src/view/mutableTableRow.ts
 var deleteBtn = document.getElementById("deletebtn");
@@ -229,7 +63,7 @@ function resetFocusedRow() {
   focusedRow = null;
 }
 __name(resetFocusedRow, "resetFocusedRow");
-function makeEditableRow(kvCache2) {
+function makeEditableRow(kvCache) {
   const rows = document.querySelectorAll("tr");
   for (const row of Array.from(rows)) {
     if (row.className.startsWith("headerRow")) continue;
@@ -254,7 +88,7 @@ function makeEditableRow(kvCache2) {
       focusedCell.onblur = () => {
         let key = focusedRow.dataset.cache_key;
         const col = focusedCell.dataset.column_id || 0;
-        const rowObj = kvCache2.get(key);
+        const rowObj = kvCache.get(key);
         const currentValue = rowObj[col];
         const thisValue = focusedCell.textContent;
         if (currentValue !== thisValue) {
@@ -262,13 +96,13 @@ function makeEditableRow(kvCache2) {
           if (col === "host") {
             const newKey = thisValue;
             if (key !== newKey) {
-              kvCache2.delete(key);
+              kvCache.delete(key);
               key = thisValue;
-              kvCache2.set(key, rowObj);
+              kvCache.set(key, rowObj);
             }
           }
         } else {
-          kvCache2.set(key, rowObj);
+          kvCache.set(key, rowObj);
         }
       };
     };
@@ -280,21 +114,21 @@ __name(makeEditableRow, "makeEditableRow");
 // src/view/customDataTable.ts
 var tablehead = document.getElementById("table-head");
 var tableBody;
-function buildTableHead(kvCache2) {
+function buildTableHead(kvCache) {
   const tr = `
 <tr class="headerRow">
 `;
   let th = "";
-  for (let i = 0; i < kvCache2.columns.length; i++) {
+  for (let i = 0; i < kvCache.columns.length; i++) {
     if (i === 1) {
       th += `    <th id="header${i + 1}" 
-   data-index=${i} value=1> ${kvCache2.columns[i].name} 
+   data-index=${i} value=1> ${kvCache.columns[i].name} 
    <span class="indicator">\u{1F53D}</span>
 </th>
 `;
     } else {
       th += `    <th id="header${i + 1}" 
-   data-index=${i} value=1> ${kvCache2.columns[i].name} 
+   data-index=${i} value=1> ${kvCache.columns[i].name} 
 </th>
 `;
     }
@@ -303,20 +137,20 @@ function buildTableHead(kvCache2) {
   tablehead.innerHTML += `</tr>`;
 }
 __name(buildTableHead, "buildTableHead");
-function buildDataTable(kvCache2) {
+function buildDataTable(kvCache) {
   if (!tableBody) {
     tableBody = document.getElementById("table-body");
   }
-  const querySet = kvCache2.querySet;
+  const querySet = kvCache.querySet;
   tableBody.innerHTML = "";
   if (querySet) {
     for (let i = 0; i < querySet.length; i++) {
       const obj = querySet[i];
-      let row = `<tr data-cache_key="${obj[kvCache2.columns[0].name]}">
+      let row = `<tr data-cache_key="${obj[kvCache.columns[0].name]}">
         `;
-      for (let i2 = 0; i2 < kvCache2.columns.length; i2++) {
-        const ro = kvCache2.columns[i2].readOnly ? " read-only" : "";
-        row += `<td data-column_id="${kvCache2.columns[i2].name}"${ro}>${obj[kvCache2.columns[i2].name]}</td>
+      for (let i2 = 0; i2 < kvCache.columns.length; i2++) {
+        const ro = kvCache.columns[i2].readOnly ? " read-only" : "";
+        row += `<td data-column_id="${kvCache.columns[i2].name}"${ro}>${obj[kvCache.columns[i2].name]}</td>
             `;
       }
       row += "</tr>";
@@ -324,29 +158,29 @@ function buildDataTable(kvCache2) {
     }
   }
   resetFocusedRow();
-  buildFooter(kvCache2);
-  makeEditableRow(kvCache2);
+  buildFooter(kvCache);
+  makeEditableRow(kvCache);
 }
 __name(buildDataTable, "buildDataTable");
 var addBtn2 = document.getElementById("addbtn");
 var deleteBtn2 = document.getElementById("deletebtn");
 var table = document.getElementById("table");
-function buildFooter(kvCache2) {
+function buildFooter(kvCache) {
   addBtn2.onclick = (_e) => {
-    const newRow = Object.assign({}, kvCache2.schema.sample);
-    kvCache2.set(newRow.host, newRow);
-    buildDataTable(kvCache2);
+    const newRow = Object.assign({}, kvCache.schema.sample);
+    kvCache.set(newRow.host, newRow);
+    buildDataTable(kvCache);
     const lastRow = table.rows[table.rows.length - 1];
     lastRow.scrollIntoView({ behavior: "smooth" });
   };
   deleteBtn2.onclick = (_e) => {
     const id = focusedRow.dataset.cache_key;
-    kvCache2.delete(id);
-    buildDataTable(kvCache2);
+    kvCache.delete(id);
+    buildDataTable(kvCache);
   };
 }
 __name(buildFooter, "buildFooter");
-signal.on("buildDataTable", "", (cache) => {
+signals.on("buildDataTable", "", (cache) => {
   buildDataTable(cache);
 });
 
@@ -361,17 +195,17 @@ var pinInput = $("pin");
 var popupText = $("popup_text");
 var pinTryCount = 0;
 var pinOK = false;
-function initDOM(kvCache2) {
-  buildTableHead(kvCache2);
+function initDOM(kvCache) {
+  buildTableHead(kvCache);
   document.addEventListener("keydown", function(event) {
     if (event.ctrlKey && event.key === "b") {
       event.preventDefault();
-      if (kvCache2.CTX.DEV) console.log("Ctrl + B backup data");
-      backupData(kvCache2);
+      if (kvCache.CTX.DEV) console.log("Ctrl + B backup data");
+      backupData(kvCache);
     }
     if (event.ctrlKey && event.key === "r") {
       event.preventDefault();
-      if (kvCache2.CTX.DEV) console.log("Ctrl + R restore data");
+      if (kvCache.CTX.DEV) console.log("Ctrl + R restore data");
       restoreData();
     }
   });
@@ -397,10 +231,10 @@ function initDOM(kvCache2) {
     event.preventDefault();
     const pinIn = pinInput;
     const pinDia = pinDialog;
-    const ecriptedPin = xorEncrypt(pinIn.value);
-    if (event.key === "Enter" || ecriptedPin === kvCache2.CTX.PIN) {
+    const ecriptedPin = signals.xorEncrypt(pinIn.value);
+    if (event.key === "Enter" || ecriptedPin === kvCache.CTX.PIN) {
       pinTryCount += 1;
-      if (ecriptedPin === kvCache2.CTX.PIN) {
+      if (ecriptedPin === kvCache.CTX.PIN) {
         pinIn.value = "";
         pinOK = true;
         pinDia.close();
@@ -420,7 +254,7 @@ function initDOM(kvCache2) {
       }
     }
   });
-  if (kvCache2.CTX.BYPASS_PIN) {
+  if (kvCache.CTX.BYPASS_PIN) {
     pinOK = true;
   } else {
     pinDialog.showModal();
@@ -428,6 +262,169 @@ function initDOM(kvCache2) {
   }
 }
 __name(initDOM, "initDOM");
+function backupData(kvCache) {
+  const jsonData = JSON.stringify(Array.from(kvCache.dbMap.entries()));
+  const link = document.createElement("a");
+  const file = new Blob([jsonData], { type: "application/json" });
+  link.href = URL.createObjectURL(file);
+  link.download = "backup.json";
+  link.click();
+  URL.revokeObjectURL(link.href);
+}
+__name(backupData, "backupData");
+function restoreData() {
+  const fileload = document.getElementById("fileload");
+  fileload?.click();
+  fileload?.addEventListener("change", function() {
+    const reader = new FileReader();
+    reader.onload = function() {
+      signals.fire("restoreCache", "", reader.result);
+      globalThis.location.reload();
+    };
+    reader.readAsText(fileload.files[0]);
+  });
+}
+__name(restoreData, "restoreData");
+
+// src/data/kvClient.ts
+var KvClient = class {
+  static {
+    __name(this, "KvClient");
+  }
+  DEV = false;
+  nextMsgID = 0;
+  querySet = [];
+  transactions = /* @__PURE__ */ new Map();
+  currentPage = 1;
+  focusedRow = null;
+  kvCache;
+  CTX;
+  ServiceURL;
+  RegistrationURL;
+  /** ctor */
+  constructor(cache, ctx) {
+    this.CTX = ctx;
+    this.DEV = ctx.DEV;
+    this.ServiceURL = ctx.LOCAL_DB ? ctx.LocalDbURL : ctx.RemoteDbURL;
+    this.RegistrationURL = this.ServiceURL + ctx.RpcURL;
+    this.kvCache = cache;
+    this.transactions = /* @__PURE__ */ new Map();
+  }
+  /** initialize our EventSource and fetch some data */
+  init() {
+    const eventSource = new EventSource(this.RegistrationURL);
+    console.log("CONNECTING");
+    eventSource.addEventListener("open", () => {
+      if (this.DEV) console.log("setting pin");
+      this.callProcedure(this.ServiceURL, "GET", { key: ["PIN"] }).then((result) => {
+        if (this.DEV) console.log("GET PIN ", result.value);
+        const pin = signals.xorEncrypt(result.value);
+        if (this.DEV) console.log("GET PIN ", pin);
+        this.CTX.PIN = result.value;
+        this.fetchQuerySet();
+      });
+    });
+    eventSource.addEventListener("error", (_e) => {
+      switch (eventSource.readyState) {
+        case EventSource.OPEN:
+          console.log("CONNECTED");
+          break;
+        case EventSource.CONNECTING:
+          console.log("CONNECTING");
+          break;
+        case EventSource.CLOSED:
+          console.log("DISCONNECTED");
+          break;
+      }
+    });
+    eventSource.addEventListener("message", (evt) => {
+      const parsed = JSON.parse(evt.data);
+      const { txID, error, result } = parsed;
+      if (txID === -1) {
+        this.handleMutation(result);
+      }
+      if (!this.transactions.has(txID)) return;
+      const transaction = this.transactions.get(txID);
+      this.transactions.delete(txID);
+      if (transaction) transaction(error, result);
+    });
+  }
+  /**
+   * handle Mutation Event
+   * @param {{ rowID: any; type: any; }} result
+   */
+  handleMutation(result) {
+    console.info(`Mutation event:`, result.type);
+  }
+  /** set Kv Pin */
+  async setKvPin(rawpin) {
+    const pin = signals.xorEncrypt(rawpin);
+    await this.callProcedure(this.ServiceURL, "SET", { key: ["PIN"], value: pin }).then((_result) => {
+      if (this.DEV) console.log(`Set PIN ${rawpin} to: `, pin);
+    });
+  }
+  /** fetch a querySet */
+  async fetchQuerySet() {
+    await this.callProcedure(
+      this.ServiceURL,
+      "GET",
+      { key: [this.CTX.dbOptions.schema.dbKey] }
+    ).then((result) => {
+      this.kvCache.restoreCache(signals.xorEncrypt(result.value));
+    });
+  }
+  /** get row from key */
+  get(key) {
+    for (let index = 0; index < this.querySet.length; index++) {
+      const element = this.querySet[index];
+      if (element.id === key) return element;
+    }
+  }
+  /** The `set` method mutates - will call the `persist` method. */
+  set(value) {
+    try {
+      this.callProcedure(
+        this.ServiceURL,
+        "SET",
+        {
+          key: [this.CTX.dbOptions.schema.dbKey],
+          value
+        }
+      ).then((result) => {
+        this.querySet = result.querySet;
+        return this.querySet;
+      });
+    } catch (e) {
+      return { Error: e };
+    }
+  }
+  /** 
+   * Make an Asynchronous Remote Proceedure Call
+   *  
+   * @param {any} procedure - the name of the remote procedure to be called
+   * @param {any} params - appropriately typed parameters for this procedure
+   * 
+   * @returns {Promise<any>} - Promise object has a transaction that is stored by ID    
+   *   in a transactions Set.   
+   *   When this promise resolves or rejects, the transaction is retrieves by ID    
+   *   and executed by the promise. 
+   */
+  callProcedure(dbServiceURL, procedure, params) {
+    const txID = this.nextMsgID++;
+    return new Promise((resolve, reject) => {
+      this.transactions.set(txID, (error, result) => {
+        if (error)
+          return reject(new Error(error));
+        resolve(result);
+      });
+      fetch(dbServiceURL, {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({ txID, procedure, params })
+      });
+    });
+  }
+};
 
 // src/data/kvCache.ts
 var KvCache = class {
@@ -444,16 +441,33 @@ var KvCache = class {
   dbMap;
   raw = [];
   CTX;
+  DEV;
   /** ctor */
   constructor(ctx) {
     this.dbKey = `${ctx.dbOptions.schema.dbKey}`;
     this.schema = ctx.dbOptions.schema;
     this.CTX = ctx;
+    this.DEV = this.CTX.DEV;
     this.callbacks = /* @__PURE__ */ new Map();
     this.dbMap = /* @__PURE__ */ new Map();
     this.columns = this.buildColumnSchema(this.schema.sample);
     this.kvClient = new KvClient(this, ctx);
     this.kvClient.init();
+    signals.on("restoreCache", "", (result) => {
+      this.restoreCache(result);
+    });
+  }
+  /** 
+   * restores our cache from a json string 
+   */
+  restoreCache(records) {
+    const pwaObj = JSON.parse(records);
+    this.dbMap = new Map(pwaObj);
+    this.persist();
+    const result = this.hydrate();
+    if (result == "ok") {
+      signals.fire("buildDataTable", "", this);
+    }
   }
   /**
    * extract a set of column-schema from the DB.schema object
@@ -479,19 +493,19 @@ var KvCache = class {
    * This is called for any mutation of the dbMap (set/delete)
    */
   persist(order = false) {
-    if (this.CTX.DEV) console.log("Persisting -> sorted? ", order);
+    if (this.DEV) console.log("Persisting -> sorted? ", order);
     if (order) {
       this.dbMap = new Map([...this.dbMap.entries()].sort());
     }
     const mapString = JSON.stringify(Array.from(this.dbMap.entries()));
-    const encrypted = xorEncrypt(mapString);
+    const encrypted = signals.xorEncrypt(mapString);
     this.kvClient.set(encrypted);
   }
   /** hydrate a dataset from a single raw record stored in kvDB */
   hydrate() {
     this.raw = [...this.dbMap.values()];
     this.querySet = [...this.raw];
-    signal.fire("buildDataTable", "", this);
+    signals.fire("buildDataTable", "", this);
     return this.raw.length > 2 ? "ok" : "Not found";
   }
   /** resest the working querySet to original DB values */
